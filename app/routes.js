4b3263f0b3244f1f9dd61f31368f64e0166e9b9b
Eddie Shannon <55087558+Ed209uk@users.noreply.github.com>
2025-06-16 13:41:16 +0100

    Attempt at routing
    Work in progress routing

--- app/routes.js
+++ app/routes.js
@@ -6,3 +6,12 @@ const router = express.Router();
 // Add your routes here - above the module.exports line
 
 module.exports = router;
+
+router.post('/relatives-with-cancer', function(request, response) {
+    var relativesCancer = request.session.data['relativesCancer']
+    if (relativesCancer == "Yes"){
+        response.redirect("/relatives-age-when-diagnosed")
+    } else {
+        response.redirect("/have-you-smoked-regularly")
+    }
+})
