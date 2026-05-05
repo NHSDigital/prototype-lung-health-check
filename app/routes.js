265d15daa28befbe3ba3e9ab49e47374b121ac7c
Simon Whatley <simonwhatley@gmail.com>
2026-05-05 16:09:33 +0100

    Add reference to prototype_v4

--- app/routes.js
+++ app/routes.js
@@ -6,6 +6,7 @@ const router = express.Router()
 router.use(require('./prototype_v1/routes'))
 router.use(require('./prototype_v2/routes'))
 router.use(require('./prototype_v3/routes'))
+router.use(require('./prototype_v4/routes'))
 
 // Add your routes here - above the module.exports line
 
